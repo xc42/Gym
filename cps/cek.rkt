f43565ce59b7c1f4b494290b3aa50bda2d476c69
xc42 <2042292281@qq.com>
2022-11-22 00:18:28 +0800

    test on try catch

--- cps/cek.rkt
+++ cps/cek.rkt
@@ -1,5 +1,6 @@
 #lang racket
 (require rackunit)
+(require rackunit/text-ui)
 
 ;;AST
 (struct Env (kvs))
@@ -7,8 +8,11 @@
 
 ;;Continuation
 (struct IdK ())
+(struct IfK (thn els env cont))
 (struct LetK (vs es body env cont))
 (struct AppK (args vals env cont))
+(struct TryK (catch-var catch-e env cont))
+(struct ThrowK (saved-cont))
 
 (define (extend-env env k v)
   (match env
@@ -33,6 +37,19 @@
 		  (apply-proc (car vals-r) (cdr vals-r) k))]
 	   [else 
 		 (interp-cps (car args) env (AppK (cdr args) (cons val vals) env k))])]
+	[(IfK thn els env cont)
+	 (if val
+	   (interp-cps thn env cont)
+	   (interp-cps els env cont))]
+	[(TryK var catch-e env cont) (apply-cont cont val)]
+	[(ThrowK k) 
+	 (let find-handler ([saved-cont k])
+	   (match saved-cont
+		 [(TryK v catch-e env cont) (interp-cps catch-e (extend-env env v val) cont)]
+		 [(? IdK?) (error "unhandled exception")]
+		 [(LetK _ _ _ _ k) (find-handler k)]
+		 [(AppK _ _ _ k) (find-handler k)]))]
+
 	))
 
 (define (apply-proc proc arg-vals cont)
@@ -53,20 +70,45 @@
 	[`(lambda ,ps ,body) (apply-cont cont (Closure ps body env))]
 	[`(let ([,vs ,es] ...) ,body)
 	  (interp-cps (car es) env (LetK vs (cdr es) body env cont))]
+	[`(if ,pred ,thn ,els) (interp-cps pred env (IfK thn els env cont))]
+	[`(try ,try-e catch (,var) ,catch-e)
+	  (interp-cps try-e env (TryK var catch-e env cont))]
+	[`(throw ,exn)
+	  (interp-cps exn env (ThrowK cont))]
 	[`(,f ,args ...) 
 	  (interp-cps f env (AppK args '() env cont))]
 	))
 
 
 (module+ test
-  (test-begin
-	(let* ([env0 (Env `((+ . ,+) (- . ,-) (* . ,*) (/ . ,/)))]
-		   [k0 (IdK)]
-		   [interp (lambda (e) (interp-cps e env0 k0))])
-	  (check-equal? (interp '2) 2)
-	  (check-equal? (interp '(+ 2 3)) 5)
-	  (check-equal? (interp '(let ([x 3]) (+ x 4))) 7)
-	  (check-equal? (interp '(let ([x 1] [y 3]) (- y x))) 2)
-	  (check-equal? (interp '(let ([db (lambda (x) (* x x))]) (db 4))) 16)
-	  (check-equal? (interp '(let ([x 3]) (let ([add3 (lambda (y) (+ y x))]) (add3 5)))) 8)
-	  )))
+  (define env0 (Env `((+ . ,+) (- . ,-) (* . ,*) (/ . ,/) 
+					  (> . ,>) (< . ,<) (>= . ,>=) (<= . ,<=) (= . ,=))))
+  (define k0 (IdK))
+  (define (ckeq e val) (check-equal? (interp-cps e env0 k0) val))
+
+  (run-tests
+	(test-suite 
+	  "CEK"
+	  (test-case 
+		"basic lang"
+		(ckeq '2 2)
+		(ckeq '(+ 2 3) 5)
+		(ckeq '(let ([x 3]) (+ x 4)) 7)
+		(ckeq '(let ([x 1] [y 3]) (- y x)) 2)
+		(ckeq '(let ([db (lambda (x) (* x x))]) (db 4)) 16)
+		(ckeq '(let ([x 3]) (let ([add3 (lambda (y) (+ y x))]) (add3 5))) 8))
+
+	  (test-case 
+		"try-catch"
+		(check-equal? (interp-cps '(try (+ 4 3) catch (ex) ex) env0 k0) 7)
+		(ckeq '(try (+ 4 3) catch (ex) ex) 7)
+		(ckeq '(let ([x 3]) (try (throw (* x x)) catch (ex) ex)) 9)
+		(ckeq '(let ([f (lambda (x)
+						  (throw x))])
+				 (try (f 41)
+					  catch (x) (+ x 1))) 42)
+		(ckeq '(let ([g (lambda (x) (if (< x 0) (throw x) x))])
+				 (let ([f (lambda (x) (+ 4 (g x)))])
+				   (try (f -3) catch (ex) (- ex)))) 3))
+	  ))
+  )
