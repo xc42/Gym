57baa6cd1ec3426469156661e7b9046dee5a23fb
xc42 <2042292281@qq.com>
2023-12-30 11:56:12 +0800

    fix coord

--- RayTracing/app/Main.hs
+++ RayTracing/app/Main.hs
@@ -6,4 +6,4 @@ import Graphics.Image.IO (writeImageExact)
 import Linear.V3
 
 main :: IO ()
-main = writeImageExact PNG [] "world.png" (renderWorld 0.0 (16/9) 16 800 1.0 (Sphere (V3 0.0 0.0 10.0) 3.0))
+main = writeImageExact PNG [] "world.png" (renderWorld 0.0 1.0 16 800 1.0 (Sphere (V3 0.0 0.0 10.0) 3.0))
