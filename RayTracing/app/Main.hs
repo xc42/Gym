afcb064b5eb6eaf4c4bcb913036825de50a40ed5
xc42 <2042292281@qq.com>
2024-01-06 14:16:20 +0800

    add materil & render multiple object

--- RayTracing/app/Main.hs
+++ RayTracing/app/Main.hs
@@ -7,6 +7,10 @@ import Linear.V3
 
 main :: IO ()
 main = do 
-    let objs = [Sphere (V3 0.0 0.0 13.0) 3.0, Sphere (V3 0.0 (-103.5) 13.0) 100]
+    let objs = [--Sphere (V3 0.0 0.0 13.0) 3.0 (Metal $ V3 0.8 0.8 0.7)
+               --, 
+               Sphere (V3 0.0 (-103.5) 13.0) 50.0 (Metal $ V3 0.8 0.7 0.7)
+               ]
+    --img  <- renderWorld 0.0 (16/9) 16 16 10.0 objs
     img  <- renderWorld 0.0 (16/9) 16 800 10.0 objs
     writeImageExact PNG [] "world.png" img
--- RayTracing/src/RayTracing.hs
+++ RayTracing/src/RayTracing.hs
@@ -6,42 +6,64 @@ import Linear.V3
 import Linear.Vector ( (^*), (*^))
 import Linear.Metric 
 
-import Graphics.Image as Img hiding (normalize)
+import Graphics.Image as Img hiding (normalize, sum)
 import Graphics.Image.Interface (makeImageM)
 
 import System.Random.Stateful
 import Debug.Trace
+import Text.Printf
 
 type V3D = V3 Double
 type Point3f = V3 Double
 
 data Ray = Ray {oringin :: Point3f,direction :: V3D}
+    deriving (Show)
 
 data Hit = Hit {hitPoint :: Point3f, normal :: V3D, frontHit :: Bool}
     deriving (Show)
 
 
-class Object o where
-    hitTest :: Ray -> o -> Maybe Hit
+
+data Object = Sphere { center :: Point3f, radius :: Double, material :: Material}
+    deriving (Show)
+
+data Material = Lambertian V3D | Metal V3D
+    deriving (Show)
 
 
-data Sphere = Sphere { center :: Point3f, radius :: Double }
+hitTests :: Ray -> [Object] -> Maybe (Hit, Object)
+hitTests _ [] = Nothing
+hitTests ray@(Ray r0 dir0) (obj:rest) =
+    let hit0 = hitTest ray obj
+        hit1 = hitTests ray rest
+     in case (hit0, hit1) of
+          (Nothing, Nothing) -> Nothing
+          (Just h0, Nothing) -> Just (h0, obj)
+          (Nothing, Just _) -> hit1
+          (Just h0@(Hit hp0 _ _), Just (Hit hp1 _ _, _)) -> 
+              let l0 = (hp0 - r0) `dot` (hp0 - r0)
+                  l1 = (hp1 - r0) `dot` (hp1 - r0)
+               in if l0 <= l1 then Just (h0, obj) else hit1
+    where hitTest (Ray rA rb) obj@(Sphere sC r _) =
+            let a = rb `dot` rb
+                b = rb `dot` (rA - sC)
+                c = (rA - sC) `dot` (rA - sC) - r * r
+                disc =  b * b - a * c
+                t = (-b - sqrt disc) / a
+                hp = rA + (t *^ rb)
+             in if disc < 0 || t <= 0
+                   then Nothing 
+                   else Just $ Hit hp (signorm (hp - sC)) True
 
-instance Object Sphere where
-    hitTest (Ray rA rb) (Sphere sC r) =
-        let a = rb `dot` rb
-            b = rb `dot` (rA - sC)
-            c = (rA - sC) `dot` (rA - sC) - r * r
-            disc =  b * b - a * c
-        in if disc < 0 
-              then Nothing 
-              else let t = (-b - sqrt disc) / a
-                       hp = rA + t *^ rb
-                    in Just $ Hit hp (signorm (hp - sC)) False
 
+scatter :: Ray -> Hit -> Material -> IO (Ray, V3D)
+scatter (Ray _ dir) (Hit hp nm _) (Metal albedo) = return (Ray hp (dir - 2.0 * (dir `dot` nm) *^ nm), albedo)
+scatter (Ray _ _) (Hit hp nm _) (Lambertian albedo) = randomNormalV3 >>= \randDir -> return (Ray hp (randDir + nm), albedo)
 
+getObjMaterial :: Object -> Material
+getObjMaterial (Sphere _ _ m) = m
 
-renderWorld :: (Object o) => Point3f -> Double -> Int -> Int -> Double -> [o] -> IO (Image VS RGB Double)
+renderWorld :: Point3f -> Double -> Int -> Int -> Double -> [Object] -> IO (Image VS RGB Double)
 renderWorld camera aspectRatio vpW imgW focal objs = 
     let vpH = fromIntegral vpW / aspectRatio
         imgH = fromIntegral imgW / aspectRatio
@@ -61,17 +83,22 @@ renderWorld camera aspectRatio vpW imgW focal objs =
             let sampleN = 10
             rays <- sampleRay row col sampleN
             let ratio = fromIntegral row / realToFrac imgH
-            let bgColor = V3 (0.6*ratio) (0.8*ratio) (1.0*ratio)
-            let (V3 r g b) = 1.0 / fromIntegral sampleN * foldl (\acc r -> acc + renderObj r objs bgColor) (V3 0.0 0.0 0.0) rays
+            -- let bgColor = V3 (0.6*ratio) (0.8*ratio) (1.0*ratio)
+            let bgColor = V3 1.0 1.0 1.0
+            let maxBounce = 50
+            colors <- sequence [renderObj r objs maxBounce bgColor| r <- rays]
+            let V3 r g b = (1.0 / fromIntegral (length colors)) *^ sum colors
             return $ PixelRGB r g b
     in  makeImageM (round imgH, imgW) renderPixel
     where 
-        renderObj :: (Object o) => Ray -> [o] -> V3D -> V3D
-        renderObj _ [] bgColor = bgColor
-        renderObj ray@(Ray origin dir) (obj:rest) bgColor = case hitTest ray obj of
-                                                      Just (Hit p nm front) -> let intensity = negate $ realToFrac $ signorm dir `dot` nm in
-                                                                               V3 intensity intensity intensity
-                                                      Nothing -> renderObj ray rest bgColor
+        renderObj :: Ray -> [Object] -> Int -> V3D -> IO V3D
+        renderObj ray@(Ray origin dir) objs bounce bgColor
+          | bounce == 0 = return $ V3 0.0 0.0 0.0
+          | otherwise = case hitTests ray objs of
+                          Just (hit, obj) ->  do 
+                              (ray', attenuation) <- scatter ray hit (getObjMaterial obj)
+                              fmap (* attenuation) (renderObj ray' objs (bounce - 1) bgColor)
+                          Nothing -> return bgColor
 
 
 uniform01 :: IO Double
